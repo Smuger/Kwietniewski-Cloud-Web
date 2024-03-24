98c649a13b9fabb1009eaedadfd492de732b6f2c
smuger <krzysiek.kwietniewski@gmail.com>
2024-03-24 20:24:44 +0000

    Updates

---
+++ component---src-pages-404-tsx-f5ef88160f1a2092a97c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkKwietniewski=self.webpackChunkKwietniewski||[]).push([[453],{731:function(e,t,a){a.r(t);var l=a(6540),n=a(1010),r=a(1238),i=a(7500);t.default=e=>{let{data:t,path:a}=e;return l.createElement(l.Fragment,null,l.createElement(n.A,{title:"404"}),l.createElement(r.o,{h:"calc(100vh)",bgGradient:"linear(to-r, #FFFF5A, #ce8b54)"},l.createElement(i.E,{color:"whiteAlpha.900",fontSize:"6xl"},"404")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-f5ef88160f1a2092a97c.js.map
